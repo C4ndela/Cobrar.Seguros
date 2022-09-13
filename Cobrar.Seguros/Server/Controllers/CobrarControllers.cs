b557a4a8c5e7b85c0d58942634c2bf7738e7f5f8
c4ndela <ds21tn.contreras.candela@gmail.com>
2022-09-12 22:35:00 -0300

    Get

---
+++ Cobrar.Seguros/Server/Controllers/CobrarControllers.cs
@@ -0,0 +1,29 @@
+﻿using CobrarSeguros.BD.Data;
+using CobrarSeguros.BD.Data.entidades;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cobrar.Seguros.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Cobrar")]
+    [Authorize]
+
+    public class CobrarControllers : ControllerBase
+    {
+        private readonly BDcontext contex;
+
+        public CobrarControllers (BDcontext contex )
+        {
+            this.contex = contex;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Poliza>>> Get()
+        {
+            return await contex.Polizas.ToListAsync(); 
+        }
+
+    }
+}
